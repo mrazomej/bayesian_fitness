c110522074d7bcc10650c911d492c3aaaf8d2914
mrazomej <manuel.razo.m@gmail.com>
2023-02-16 15:29:26 -0800

    update package and viz.jl module

--- BayesFitness/src/viz.jl
+++ BayesFitness/src/viz.jl
@@ -1,5 +1,6 @@
 using Measures, CairoMakie
 import ColorSchemes
+import MCMCChains
 
 """
     `pboc_plotlyjs!()`
@@ -174,3 +175,80 @@ function pboc_makie!()
     )
     set_theme!(theme)
 end
+
+@doc raw"""
+    `mcmc_trace_density!(fig::Figure, chain::MCMCChains.Chains; colors, labels)`
+Function to plot the traces and density estimates side-to-side for each of the
+parametres in the `MCMCChains.Chains` object.
+# Arguments
+- `fig::Figure`: Figure object to be populated with plot. This allows the user
+  to decide the size of the figure outside of this function.
+- `chain::MCMCChains.Chains`: Samples from the MCMC run generated with
+  Turing.jl.
+## Optional arguments
+- `colors`: List of colors to be used in plot.
+- `labels`: List of labels for each of the parameters. If not given, the default
+  will be to use the names stored in the MCMCChains.Chains object
+- `alpha::AbstractFloat=1`: Level of transparency for plots.
+"""
+function mcmc_trace_density!(
+    fig::Figure,
+    chain::MCMCChains.Chains;
+    colors=ColorSchemes.seaborn_colorblind,
+    labels=[],
+    alpha::AbstractFloat=1.0
+)
+    # Extract parameters
+    params = names(chain, :parameters)
+    # Extract number of chains
+    n_chains = length(MCMCChains.chains(chain))
+    # Extract number of parameters
+    n_samples = length(chain)
+
+    # Check that the number of given labels is correct
+    if (length(labels) > 0) & (length(labels) != length(params))
+        error("The number of lables must match number of parameters")
+    end # if
+
+    # Check that the number of given colors is correct
+    if length(colors) < n_chains
+        error("Please give at least as many colors as chains in the MCMC")
+    end # if
+
+    # Loop through parameters
+    for (i, param) in enumerate(params)
+        # Check if labels were given
+        if length(labels) > 0
+            lab = labels[i]
+        else
+            lab = string(param)
+        end # if
+        # Add axis for chain iteration
+        ax_trace = Axis(fig[i, 1]; ylabel=lab)
+        # Inititalize axis for density plot
+        ax_density = Axis(fig[i, 2]; ylabel=lab)
+        # Loop through chains
+        for chn in 1:n_chains
+            # Extract values
+            values = chain[:, param, chn]
+            # Plot traces of walker
+            lines!(ax_trace, 1:n_samples, values, color=(colors[chn], alpha))
+            # Plot density
+            density!(ax_density, values, color=(colors[chn], alpha))
+        end # for
+
+        # Hide y-axis decorations
+        hideydecorations!(ax_trace; label=false)
+        hideydecorations!(ax_density; label=false)
+
+        # Check if it is bottom plot
+        if i < length(params)
+            # hide x-axis decoratiosn
+            hidexdecorations!(ax_trace; grid=false)
+        else
+            # add x-label
+            ax_trace.xlabel = "iteration"
+            ax_density.xlabel = "parameter estimate"
+        end # if
+    end # for
+end # function
