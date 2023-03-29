e40ffeb130d223cd057da57f846cbad208710ab5
mrazomej <manuel.razo.m@gmail.com>
2023-03-29 16:26:47 -0700

    add copy of startup.jl file to miscellaneous

--- miscellaneous/README.md
+++ miscellaneous/README.md
@@ -1,3 +1,17 @@
 ## `miscellaneous`
 
-While the other directories in this repository are focused on code and data, this folder can serve as laboratory notebook where you can write down various details about your project. I find it useful to break this up by section where I can easily share a direct link if someone asks for a protocol. 
+### `startup.jl`
+Script containing necessary functions to easily import the project software
+package (`BayesFitUtils`) from any directory within the repository.
+
+To add this functionality, follow these steps:
+1. Add the `Suppressor.jl` package to your main `Julia` installation by typing
+```
+add Suppressor
+```
+from the `Pkg` manager in the `Julia REPL`.
+
+2. copy this script to the directory
+```
+~/.julia/config/
+```
---
+++ miscellaneous/startup.jl
@@ -0,0 +1,50 @@
+# Import package to supress warning
+import Suppressor
+
+# Import package manager
+Suppressor.@suppress import Pkg
+
+@doc raw"""
+    `git_root()`
+
+Function to find the root environment of a particular Git repository
+"""
+function git_root()
+    # Load package for project by locating git home folder
+    home_dir = read(`git rev-parse --show-toplevel`, String)
+    return home_dir[1:end-1]
+end # function
+
+@doc raw"""
+    `load_pkg(pkg)`
+
+Function to find the home directory of a github repository to load the package
+for that particular project.
+
+# Arguments
+- `pkg::String`: Name of the folder within the `GitHub` repository where the
+  package is located.
+"""
+function load_pkg(pkg::String)
+    # Suppress warning
+    Suppressor.@suppress begin
+        # Save current directory
+        dir = read(`pwd`, String)
+        dir = dir[1:end-1]
+
+        # Change directories
+        cd(git_root() * "/" * string(pkg))
+
+        # Activate environment
+        Pkg.pkg"activate ."
+
+        # Return to original directory
+        cd(dir)
+    end # @suppress
+
+end # function
+
+macro load_pkg(pkg)
+    load_pkg(pkg)
+    return nothing
+end # marcro
