c7e2000ebb069c87090ee13b9220a4fe40330eb5
mrazomej <manuel.razo.m@gmail.com>
2023-06-14 09:43:26 -0700

    scripts to process the Kinsler datasets in different modalities with the joint inference model

---
+++ code/processing/mcmc_kinsler_fitness/mcmc_joint_inference_multiprocess.jl
@@ -0,0 +1,102 @@
+#
+
+# Activate environment
+@load_pkg BayesFitUtils
+
+# Define number of processes
+n_procs = 4
+
+# Import Distributed to add processes and the @everywhere macro.
+import Distributed
+
+println("Adding multiple processess...\n")
+# Add processes
+Distributed.addprocs(n_procs; exeflags="--project=$(Base.active_project())")
+
+println("Number of processes: $(Distributed.nprocs())")
+println("Number of workers: $(Distributed.nworkers())\n")
+
+println("Importing packages on all processess...\n")
+# Import packages everywhere
+Distributed.@everywhere begin
+    # Import library package
+    import BayesFitness
+
+    # Import MCMC-related packages
+    import Turing
+    using ReverseDiff
+
+    # Set AutoDiff backend to ReverseDiff.jl for faster computation
+    Turing.setadbackend(:reversediff)
+    # Allow system to generate cache to speed up computation
+    Turing.setrdcache(true)
+
+    # Import libraries to manipulate data
+    import DataFrames as DF
+    import CSV
+
+    # Import library to save and load native julia objects
+    import JLD2
+
+    # Import library to list files
+    import Glob
+
+    import Random
+
+    Random.seed!(42)
+
+    # Define inference parameters
+    n_walkers = 4
+    n_steps = 1000
+end # @everywhere
+
+##
+
+# Import data
+df = CSV.read("$(git_root())/data/kinsler_2020/tidy_counts.csv", DF.DataFrame)
+
+# Read datasets visual evaluation info
+df_include = CSV.read(
+    "$(git_root())/data/kinsler_2020/exp_include.csv", DF.DataFrame
+)
+
+# Upload dataset to all processes
+@eval Distributed.@everywhere df = $df
+@eval Distributed.@everywhere df_include = $df_include
+
+##
+
+# Loop through datasets
+# @async Distributed.@distributed 
+for i = 1:size(df_include, 1)
+    # Extract info
+    env, rep, rm_T0 = collect(df_include[i, :])
+    # Extract data
+    data = df[(df.env.==env).&(df.rep.==rep), :]
+
+    println("processing $(env) | $(rep)")
+
+    # Define function parameters
+    param = Dict(
+        :data => data,
+        :n_walkers => n_walkers,
+        :n_steps => n_steps,
+        :outputname => "./output/kinsler_$(env)env_$(rep)rep_$(rm_T0)rmT0",
+        :model => BayesFitness.model.fitness_lognormal,
+        :sampler => Turing.NUTS(0.65),
+        # :ensemble => Turing.MCMCSerial(),
+        :ensemble => Turing.MCMCDistributed(),
+        :rm_T0 => rm_T0,
+        :verbose => true,
+    )
+
+    # Run inference
+    println("Running Inference for group $(i)...")
+
+    try
+        @time BayesFitness.mcmc.mcmc_joint_fitness(; param...)
+    catch
+        @warn "Group $(i) was already processed"
+    end
+
+end # for
---
+++ code/processing/mcmc_kinsler_fitness/mcmc_joint_inference_multithread.jl
@@ -0,0 +1,75 @@
+#
+
+println("Importing packages...\n")
+# Activate environment
+@load_pkg BayesFitUtils
+
+# Import library package
+import BayesFitness
+
+# Import MCMC-related packages
+import Turing
+using ReverseDiff
+
+# Set AutoDiff backend to ReverseDiff.jl for faster computation
+Turing.setadbackend(:reversediff)
+# Allow system to generate cache to speed up computation
+Turing.setrdcache(true)
+
+# Import libraries to manipulate data
+import DataFrames as DF
+import CSV
+
+# Import library to save and load native julia objects
+import JLD2
+
+# Import library to list files
+import Glob
+
+import Random
+
+Random.seed!(42)
+
+# Define inference parameters
+n_walkers = 4
+n_steps = 1000
+
+##
+
+# Import data
+df = CSV.read("$(git_root())/data/kinsler_2020/tidy_counts.csv", DF.DataFrame)
+
+# Read datasets visual evaluation info
+df_include = CSV.read(
+    "$(git_root())/data/kinsler_2020/exp_include.csv", DF.DataFrame
+)
+
+# Loop through datasets
+@async Threads.@threads for i = 1:size(df_include, 1)
+    # Extract info
+    env, rep, rm_T0 = collect(df_include[i, :])
+    # Extract data
+    data = df[(df.env.==env).&(df.rep.==rep), :]
+
+    # Define function parameters
+    param = Dict(
+        :data => data,
+        :n_walkers => n_walkers,
+        :n_steps => n_steps,
+        :outputname => "./output/kinsler_$(env)env_$(rep)rep_$(rm_T0)rmT0",
+        :model => BayesFitness.model.fitness_lognormal,
+        :sampler => Turing.NUTS(0.65),
+        :ensemble => Turing.MCMCSerial(),
+        :rm_T0 => rm_T0,
+    )
+
+    # Run inference
+    println("Running Inference for group $(i)...")
+
+    try
+        @time BayesFitness.mcmc.mcmc_joint_fitness(; param...)
+    catch
+        @warn "Group $(i) was already processed"
+    end
+
+end # for
