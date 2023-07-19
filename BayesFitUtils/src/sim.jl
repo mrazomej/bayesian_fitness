4f4d6db68a5481995f15acd1943b01e31ad23a58
mrazomej <manuel.razo.m@gmail.com>
2023-07-19 09:56:18 -0700

    add method to generate multi-environment simulations

--- BayesFitUtils/src/sim.jl
+++ BayesFitUtils/src/sim.jl
@@ -53,11 +53,11 @@ ṅᵢ = λᵢnᵢ (1 - ∑ᵢ(nᵢ)/κ),
 and a Poisson sampling at each dilution step.
 
 # Arguments
-- `λ̲::Vector{Float64}`: Vector of growth rate values for all genotypes. NOTE:
-  The reference growth rate (usually the neutral lineage) should be the *first
-  entry* of this vector.
-- `n̲₀::Vector{Int64}`: Vector with the initial frequencies for all genotypes to
-  track.
+- `λ̲::AbstractVector{Float64}`: Vector of growth rate values for all genotypes.
+  NOTE: The reference growth rate (usually the neutral lineage) should be the
+  *first entry* of this vector.
+- `n̲₀::AbstractVector{Int64}`: Vector with the initial frequencies for all
+  genotypes to track.
 
 # Optional Keyword Arguments
 - `n_cycles::Int64=4`: Number of growth-dilution cycles over which to integrate
@@ -140,6 +140,144 @@ function logistic_fitness_measurement(
         # Define ODE problem
         prob = ODEProblem(
             dndt_rhs!, n_init, t_span, (λ̲, κ)
+        )
+        # Check dilution factor
+        if typeof(dilution) <: Nothing
+            # Solve system and store final point
+            n_mat[cyc, :] = Int64.(round.(solve(prob).u[end] ./ 2^(n_gen)))
+        else
+            # Solve system and store final point
+            n_mat[cyc, :] = Int64.(round.(solve(prob).u[end] ./ dilution))
+        end # if
+    end # for
+
+    # Add Gaussian noise to each value
+    if σ_lognormal > 0.0
+        # Sample Gaussian noise for each measurement
+        n_mat[n_mat.>0] = Int64.(
+            round.(
+                exp.(
+                    first.(
+                        rand.(
+                            Distributions.Normal.(
+                                log.(n_mat[n_mat.>0]), Ref(σ_lognormal)
+                            ),
+                            1
+                        )
+                    )
+                )
+            )
+        )
+    end # if
+
+    return n_mat
+end # function
+
+@doc raw"""
+    logistic_fitness_measurement(λ̲̲, n̲₀; n_cycles, n_gen, exp_frac, κ, dilution, poisson_noise, σ_lognormal)
+
+Function to simulate fitness measurment experiments over multiple
+growth-dilution cycles using a logistic growth model of the form
+
+ṅᵢ = λᵢnᵢ (1 - ∑ᵢ(nᵢ)/κ),
+
+and a Poisson sampling at each dilution step. This method allows for different
+growth rates on each of the environments.
+
+# Arguments
+- `λ̲̲::AbstractMatrix{Float64}`: `B × E` matrix of growth rate values for all
+  `B` genotypes in all `E` unique environments. NOTE: The reference growth rate
+  (usually the neutral lineage) should be the *first entry* of this vector.
+- `n̲₀::AbstractVector{Int64}`: Vector with the initial frequencies for all
+  genotypes to track.
+- `env_idx::AbstractVector{Int64}`: Vector used to index the corresponding
+  growth rate for each cycle. The number of entries in this vector defines the
+  number of cycles.
+
+# Optional Keyword Arguments
+- `exp_frac::Float64=1/3`: Fraction of the growth-dilution cycle that cells
+  spend on exponential growth. NOTE: The total time of integration is computed
+  as
+  - `log(2) / first(λ̲̲) * n_gen / exp_frac`. **Note** that the very first entry
+  of the growth rate matrix is taken as the reference.
+- `κ::Float64=10.0^10`: Environment carrying capacity.
+- `dilution::Union{Nothing,Float64}=nothing`: Dilution factor after every growth
+  cycle. The initial condition for the next cycle are computed as
+  - `solve(prob).u[end] ./ dilution`. if `typeof(dilution) == Nothing` the
+  dilution factor is `1 / 2^n_gen`.
+- `poisson_noise::Bool=true`: Boolean indicating if Poisson noise between
+  dilutions should be added to the data.
+- `σ_lognormal::Float64=0.0`: Standard deviation for a log-normal distribution
+  that adds extra noise to th ebarcode measurements.
+
+# Returns
+- `Matrix{Int64}`: `(n_cycles + 1) × n_genotypes` matrix with the barcode count
+  trajectories for all genotypes.
+"""
+function logistic_fitness_measurement(
+    λ̲̲::AbstractMatrix{Float64},
+    n̲₀::AbstractVector{Int64},
+    env_idx::Vector{Int64};
+    n_gen::Int64=8,
+    exp_frac::Float64=1 / 3,
+    κ::Float64=10.0^10,
+    dilution::Union{Nothing,Float64}=nothing,
+    poisson_noise::Bool=true,
+    σ_lognormal::Float64=0.0
+)
+    # Check the input exp_frac
+    if !(0 < exp_frac < 1)
+        error("exp_frac must be ∈ (0, 1]")
+    end # if
+    # Check the positivity of initial populations
+    if any(n̲₀ .< 0)
+        error("Initial populations cannot be negative")
+    end # if
+    # Check that the initial population is not larger than the carrying capacity
+    if sum(n̲₀) ≥ κ
+        error("The initial population is larger than the carrying capacity")
+    end # if
+    # Check the value of the σ_lognormal
+    if σ_lognormal < 0.0
+        error("σ_lognormal is strictly ≥ 0.0")
+    end # if
+
+    # Define time span
+    t_span = (0.0, log(2) / first(λ̲̲) * (n_gen / exp_frac))
+
+    # Define number of genotypes
+    n_geno = size(λ̲̲, 1)
+
+    # Define number of unique environments
+    n_env = size(λ̲̲, 2)
+
+    # Define number of cycles
+    n_cycles = length(env_idx)
+
+    # Inititalize matrix to save output
+    n_mat = Matrix{Int64}(undef, n_cycles + 1, n_geno)
+
+    # Store initial condition
+    n_mat[1, :] = n̲₀
+
+    # Loop through cycles
+    for cyc = 2:n_cycles+1
+        # Initialize vector with inital conditions as zeros
+        n_init = zeros(length(n̲₀))
+        # Locate non-zero entries from previous cycle
+        non_zero_idx = n_mat[cyc-1, :] .> 0.0
+
+        if poisson_noise
+            # Define initial condition for next cycle with Poisson sampling.
+            n_init[non_zero_idx] = Random.rand.(
+                Distributions.Poisson.(n_mat[cyc-1, non_zero_idx])
+            )
+        else
+            n_init[non_zero_idx] = n_mat[cyc-1, non_zero_idx]
+        end # if
+        # Define ODE problem
+        prob = ODEProblem(
+            dndt_rhs!, n_init, t_span, (λ̲̲[:, env_idx[cyc-1]], κ)
         )
         # Check dilution factor
         if typeof(dilution) <: Nothing
