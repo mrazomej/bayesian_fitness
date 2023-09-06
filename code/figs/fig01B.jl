70de255d706f6423e5c9f515dc3804ac47b28b38
mrazomej <manuel.razo.m@gmail.com>
2023-09-06 08:56:35 -0700

    update dimensions

--- code/figs/fig01B.jl
+++ code/figs/fig01B.jl
@@ -50,7 +50,7 @@ color_grad = cgrad(ColorSchemes.Blues, unique(data.fitness))
 color = [x for x in color_grad]
 
 # Initialize figure
-fig = Figure(resolution=(450, 300))
+fig = Figure(resolution=(400, 250))
 
 # Add axis
 ax = Axis(fig[1, 1], xlabel="time", ylabel="barcode frequency", yscale=log10)
