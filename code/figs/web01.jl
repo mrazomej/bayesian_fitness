2c8272a3f96f7ce2c8e0ccee91d509cd27458c2c
mrazomej <manuel.razo.m@gmail.com>
2023-09-28 14:59:49 -0700

    scripts to make website figures

---
+++ code/figs/web01.jl
@@ -0,0 +1,72 @@
+# Load project package
+@load_pkg BayesFitUtils
+
+# Import package with useful plotting functions for our dataset
+import BayesFitUtils
+
+# Import Bayesian inference package
+import BayesFitness
+
+# Import libraries to manipulate data
+import DataFrames as DF
+import CSV
+
+# Import plotting libraries
+using CairoMakie
+import ColorSchemes
+
+# Activate backend
+CairoMakie.activate!()
+
+# Set PBoC Plotting style
+BayesFitUtils.viz.theme_makie!()
+
+# Import library to set random seed
+import Random
+
+Random.seed!(42)
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+# Loading data
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
+println("Loading data...")
+
+# Import data
+data = CSV.read(
+    "$(git_root())/data/logistic_growth/data_001/tidy_data.csv", DF.DataFrame
+)
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+# Plot trajectories
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
+# Initialize figure
+fig = Figure(resolution=(350, 300))
+
+# Add axis
+ax = Axis(
+    fig[1, 1], xlabel="time", ylabel="barcode frequency", yscale=log10
+)
+
+# Plot mutant barcode trajectories
+BayesFitUtils.viz.bc_time_series!(
+    ax,
+    data[.!(data.neutral), :],
+    quant_col=:freq,
+    zero_lim=0,
+    alpha=0.35
+)
+
+# Plot neutral barcode trajectories
+BayesFitUtils.viz.bc_time_series!(
+    ax,
+    data[data.neutral, :],
+    quant_col=:freq,
+    zero_lim=0,
+    color=ColorSchemes.Blues_9[end],
+)
+
+save("/Users/mrazo/git/BayesFitness/docs/src/figs/fig01.svg", fig)
+
+fig
---
+++ code/figs/web02.jl
@@ -0,0 +1,66 @@
+# Load project package
+@load_pkg BayesFitUtils
+
+# Import package with useful plotting functions for our dataset
+import BayesFitUtils
+
+# Import Bayesian inference package
+import BayesFitness
+
+# Import libraries to manipulate data
+import DataFrames as DF
+import CSV
+
+# Import plotting libraries
+using CairoMakie
+import ColorSchemes
+
+# Activate backend
+CairoMakie.activate!()
+
+# Set PBoC Plotting style
+BayesFitUtils.viz.theme_makie!()
+
+# Import library to set random seed
+import Random
+
+Random.seed!(42)
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+# Loading data
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
+println("Loading data...")
+
+# Import data
+data = CSV.read(
+    "$(git_root())/data/logistic_growth/data_001/tidy_data.csv", DF.DataFrame
+)
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+# Plot trajectories
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
+# Initialize figure
+fig = Figure(resolution=(350, 300))
+
+# Add axis
+ax = Axis(fig[1, 1], xlabel="time", ylabel="ln(fₜ₊₁/fₜ)")
+
+# Plot mutant barcode trajectories
+BayesFitUtils.viz.logfreq_ratio_time_series!(
+    ax,
+    data[.!(data.neutral), :],
+    alpha=0.3
+)
+
+# Plot neutral barcode trajectories
+BayesFitUtils.viz.logfreq_ratio_time_series!(
+    ax,
+    data[data.neutral, :],
+    color=ColorSchemes.Blues_9[end],
+)
+
+save("/Users/mrazo/git/BayesFitness/docs/src/figs/fig02.svg", fig)
+
+fig
