f768008318768b41508c7ec8e17ab0f40c069e07
mrazomej <manuel.razo.m@gmail.com>
2023-12-31 13:12:32 -0800

    Add custom LaTe and HTML blocks for maintext and sitext divs

--- doc/maintext-filter.lua
+++ doc/maintext-filter.lua
@@ -1,8 +1,10 @@
 function Div(el)
-    -- Check if the div has a specific class, e.g., 'maintext'
+    -- Check if the div has the 'maintext' class
     if el.classes:includes("maintext") then
-        -- Define multi-line string for the beginning of the custom LaTeX block
-        local maintextStart = [[
+        -- Apply modifications if the output format is PDF
+        if FORMAT:match("latex") then
+            -- Define multi-line string for the beginning of the custom LaTeX block
+            local maintextStart = [[
 % Remove main text from the table of contents by specifying not to include
 % any section or subsection
 \addtocontents{toc}{\protect\setcounter{tocdepth}{-1}}
@@ -14,8 +16,8 @@ function Div(el)
 \defbibfilter{notother}{not segment=\therefsegment}
 ]]
 
-        -- Define multi-line string for the end of the custom LaTeX block
-        local maintextEnd = [[
+            -- Define multi-line string for the end of the custom LaTeX block
+            local maintextEnd = [[
 % Print main text references
 \printbibliography[segment=\therefsegment]
 % Close reference segment
@@ -24,14 +26,23 @@ function Div(el)
 \clearpage
 ]]
 
-        -- Convert the div to a custom LaTeX block
-        local latexStart = pandoc.RawBlock('latex', maintextStart)
-        local latexEnd = pandoc.RawBlock('latex', maintextEnd)
+            -- Convert the div to a custom LaTeX block
+            local latexStart = pandoc.RawBlock('latex', maintextStart)
+            local latexEnd = pandoc.RawBlock('latex', maintextEnd)
 
-        -- Insert the LaTeX start and end commands around the div content
-        table.insert(el.content, 1, latexStart)
-        table.insert(el.content, latexEnd)
+            -- Insert the LaTeX start and end commands around the div content
+            table.insert(el.content, 1, latexStart)
+            table.insert(el.content, latexEnd)
 
-        return el.content
+            return el.content
+
+            -- Apply different modifications for HTML output
+        elseif FORMAT:match("html") then
+            -- For HTML, return the content only (ignoring the div wrapper)
+            return el.content
+        end
     end
+
+    -- For divs without the 'maintext' class or other formats, return the div unchanged
+    return el
 end
